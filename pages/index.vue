12b7c1e0d21adab9dfae6d2989fdd3a50365dae9
Chingiz Mammadov <info@chz.dev>
2023-08-25 22:29:43 +0400

    eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
+<script setup></script>
+
 <template>
   <section class="ma4 text-24px/24px text-red text-center">
-    Nuxt 3 + UnoCSS Minimal Starter <i class="i-custom:plus text-14px text-blue"></i> Custom icons
+    Nuxt 3 + UnoCSS Minimal Starter <i class="i-custom:plus text-14px text-blue" /> Custom icons
   </section>
 </template>
